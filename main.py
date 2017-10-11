e79d65d7255aaf4b7230883642c9e74144213382
Belkisa <kisacausevic@gmail.com>
2017-10-10 22:24:46 -0500

    blog functionality added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:MyPassword@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'gsr345456gfdfgdfGDFGHDF'
 
 class Blog(db.Model):
 
@@ -20,6 +21,11 @@ class Blog(db.Model):
 @app.route('/blog', methods=['POST', 'GET'])
 def blog_display():
 
+    post_id = int(request.form['post-id'])
+    p_id = Blog.query.filter_by(post_id = post.id).first()
+    post_id_get = request.args[p_id]
+    if 
+
     posts = Blog.query.all()
     return render_template('blog.html',posts=posts)
 
@@ -28,25 +34,34 @@ def blog_display():
 def newpost():
     title = ''
     body = ''
+    errors = False
 
     if request.method == 'POST':
         post_title = request.form['title']
         post_body = request.form['body']
 
         if post_title == '':
-            flash("Please enter a title")
+            flash("Please enter a title", "title_error")
             title = ''
-            return redirect('/newpost')
+            body = post_body
+            errors = True
         else:
             title = post_title
         if post_body == '':
-            flash("Please enter the body of your post")
+            flash("Please enter the body of your post", "body_error")
             body = ''
-            return redirect('/newpost')
+            title = post_title
+            errors = True
         else:
             body = post_body
+        if errors:
+            return render_template('newpost.html',title=title,body=body)
 
-        if not title and not body:
+        #if post_title == '' and post_body == '':
+        #    flash("Please enter a title")
+        #    flash("Please enter the body of your post")
+        #    return redirect("/newpost")
+        else:
             new_post = Blog(post_title,post_body)
             db.session.add(new_post)
             db.session.commit()
